288fcc70213b52d080d1d9e1719d9595ba4c5ffd
Trois25 <juanajh428@gmail.com>
2021-09-19 19:28:28 +0700

    laravel_tugas

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\FilmController;
+use App\http\Controllers\genreController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/film',[FilmController::class,'index']);
+Route::post('/film',[FilmController::class,'store']);
+
+Route::get('/film/genre',[GenreController::class,'index']);
+Route::post('/film/genre',[GenreController::class,'store']);
